f417bf953a619b6ada1d3195c3bd70f0e4007014
Mieszko Malawski <mmalawski@getresponse.com>
2017-08-23 08:19:32 +0200

    prophecy version

--- tests/Unit/CreateTest.php
+++ tests/Unit/CreateTest.php
@@ -16,48 +16,48 @@ class CreateTest extends TestCase
     /**
      * @test
      */
-    public function entityShouldBeSaved()
-    {
-        $repositoryMock = $this->getMockBuilder(Repository::class)->disableOriginalConstructor()->getMock();
-        $validatorMock = $this->getMockBuilder(Validator::class)->getMock();
-
-        $entity = new Entity('some_name');
-        $validatorMock->expects(static::any())->method('isValid')->with($entity)->willReturn(true);
-        $repositoryMock->expects(static::any())->method('save')->with($entity)->willReturn(1);
-
-        $repositoryMock->expects(static::any())->method('getById')->with(1)->willReturn($entity);
-
-        $sut = new Service(
-            $repositoryMock,
-            $validatorMock
-        );
-
-        $result = $sut->create(['name' => 'some_name']);
-
-        static::assertEquals($entity, $result);
-    }
-
 //    public function entityShouldBeSaved()
 //    {
-//        /** @var Repository $repositoryMock */
-//        $repositoryMock = $this->prophesize(Repository::class);
-//        /** @var Validator $validatorMock */
-//        $validatorMock = $this->prophesize(Validator::class);
+//        $repositoryMock = $this->getMockBuilder(Repository::class)->disableOriginalConstructor()->getMock();
+//        $validatorMock = $this->getMockBuilder(Validator::class)->getMock();
 //
 //        $entity = new Entity('some_name');
-//        $validatorMock->isValid($entity)->willReturn(true);
-//        $repositoryMock->save($entity)->will(function ($args) use ($repositoryMock) {
-//            $repositoryMock->getById(1)->willReturn($args[0]);
-//            return 1;
-//        });
+//        $validatorMock->expects(static::any())->method('isValid')->with($entity)->willReturn(true);
+//        $repositoryMock->expects(static::any())->method('save')->with($entity)->willReturn(1);
+//
+//        $repositoryMock->expects(static::any())->method('getById')->with(1)->willReturn($entity);
 //
 //        $sut = new Service(
-//            $repositoryMock->reveal(),
-//            $validatorMock->reveal()
+//            $repositoryMock,
+//            $validatorMock
 //        );
 //
 //        $result = $sut->create(['name' => 'some_name']);
 //
 //        static::assertEquals($entity, $result);
 //    }
+
+    public function entityShouldBeSaved()
+    {
+        /** @var Repository $repositoryMock */
+        $repositoryMock = $this->prophesize(Repository::class);
+        /** @var Validator $validatorMock */
+        $validatorMock = $this->prophesize(Validator::class);
+
+        $entity = new Entity('some_name');
+        $validatorMock->isValid($entity)->willReturn(true);
+        $repositoryMock->save($entity)->will(function ($args) use ($repositoryMock) {
+            $repositoryMock->getById(1)->willReturn($args[0]);
+            return 1;
+        });
+
+        $sut = new Service(
+            $repositoryMock->reveal(),
+            $validatorMock->reveal()
+        );
+
+        $result = $sut->create(['name' => 'some_name']);
+
+        static::assertEquals($entity, $result);
+    }
 }
